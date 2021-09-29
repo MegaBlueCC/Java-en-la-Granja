c78d35274d72d0ea4acf0d7939a36aadb09d0470
MegaBlueCC <felipeesparza553@gmail.com>
2021-09-28 21:49:33 -0300

    primer commit

---
+++ js/app.js
@@ -0,0 +1,91 @@
+
+let carritoDeCompras = []
+
+const contenedorProductos = document.getElementById('contenedor-productos');
+const contenedorCarrito = document.getElementById('carrito-contenedor');
+
+const contadorCarrito = document.getElementById('contadorCarrito');
+const precioTotal = document.getElementById('precioTotal');
+
+
+mostrarProductos(stockProductos)
+
+function mostrarProductos(array){   
+    array.forEach(producto => {
+        let div = document.createElement('div');
+        div.classList.add('producto');
+        div.innerHTML += `<div class="cont__cajita">
+        <div class="cajita">
+            <div class="cajita__img"><img src=${producto.img} alt="verduras"></div>
+            <p class="ventas__titulos">${producto.nombre}</p> 
+            <a class="btn-floating halfway-fab waves-effect waves-light red" id=boton${producto.id}><i class="material-icons">add_shopping_cart</i></a>  
+            <p class="ventas__titulos">${producto.desc}</p>
+            <p class="ventas__titulos">${producto.precio}</p>
+            <p class="ventas__titulos">Talle: ${producto.talle}</p>
+            
+        </div>
+    </div>
+        
+        `
+        // div.innerHTML += `<div class="card" >
+        //                     <div class="card-image">
+        //                         <img src=${producto.img}>
+        //                         <span class="card-title">${producto.nombre}</span>
+        //                         <a class="btn-floating halfway-fab waves-effect waves-light red" id=boton${producto.id}><i class="material-icons">add_shopping_cart</i></a>
+        //                     </div>
+        //                     <div class="card-content">
+        //                         <p>${producto.desc}</p>
+        //                         <p>Talle: ${producto.talle}</p>
+        //                         <p> ${producto.precio}</p>
+        //                     </div>`
+        contenedorProductos.appendChild(div);
+
+        let boton = document.getElementById(`boton${producto.id}`)
+
+        boton.addEventListener('click',()=>{
+            agregarAlCarrito(producto.id)
+        })
+
+    });
+}
+
+
+function agregarAlCarrito(id) {
+    let repetido = carritoDeCompras.find(productoR => productoR.id == id);
+    if(repetido){
+        repetido.cantidad =  repetido.cantidad + 1
+        document.getElementById(`cantidad${repetido.id}`).innerHTML = `<p id=cantidad${repetido.id}>Cantidad:${repetido.cantidad}</p>`
+        actualizarCarrito()
+    }else{
+      let productoAgregar = stockProductos.find(prod => prod.id == id);
+    console.log(productoAgregar)
+    carritoDeCompras.push(productoAgregar);
+    
+        productoAgregar.cantidad = 1;
+    let div = document.createElement('div');
+    div.classList.add('productoEnCarrito');
+    div.innerHTML= `<p>${productoAgregar.nombre}</p>
+                    <p>Precio:${productoAgregar.precio}</p>
+                    <p id=cantidad${productoAgregar.id}>Cantidad:${productoAgregar.cantidad}</p>
+                    <button id=eliminar${productoAgregar.id} class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>`
+
+    contenedorCarrito.appendChild(div)  
+    actualizarCarrito()
+    let botonEliminar = document.getElementById(`eliminar${productoAgregar.id}`)
+    botonEliminar.addEventListener('click', ()=>{
+        botonEliminar.parentElement.remove()
+        carritoDeCompras = carritoDeCompras.filter(el => el.id != productoAgregar.id);
+        actualizarCarrito();
+    })  
+    }
+    
+    
+    
+    
+}
+
+function actualizarCarrito() {
+   contadorCarrito.innerText = carritoDeCompras.reduce((acc , el)=> acc + el.cantidad,0);
+   precioTotal.innerText = carritoDeCompras.reduce((acc, el) => acc + (el.precio * el.cantidad) , 0)
+}
+
---
+++ js/stock.js
@@ -0,0 +1,10 @@
+let stockProductos = [
+    {id: 1, nombre: "Verduras", tipo: "comida", desc: "Las mejores y más frescas verduras de estación", precio: 500, talle: "-", img: 'https://i.ibb.co/Hn9xn9s/IMG-20210818-183742.jpg', cantidad:1},
+    {id: 2, nombre: "Mermelada", tipo: "comida", desc: "Riquísima mermelada de frutas frescas", precio: 400, talle: "-", img: 'https://i.ibb.co/GHxTL2Y/IMG-20210818-183839.jpg', cantidad:1},
+    {id: 3, nombre: "Gorra", tipo: "gorra", desc: "Una gorra que re va con vos", precio: 800, talle: "M", img: 'https://i.ibb.co/RBHFDrF/037352d928bbcd325e2bcacf4ea0cfd1.jpg', cantidad:1},
+    {id: 4, nombre: "Curso n°1", tipo: "capacitaciones", desc: "Un curso pensado para vos", precio: 1500, talle: "-", img: 'https://i.ibb.co/fHbbQgm/IMG-20210818-183808.jpg', cantidad:1},
+    {id: 5, nombre: "Remera", tipo: "remera", desc: "Una remera que re va con vos", precio: 1000, talle: "S", img: 'https://i.ibb.co/2Y4DCq2/IMG-20210818-183824.jpg', cantidad:1},
+    {id: 6, nombre: "Frutas", tipo: "comida", desc: "Las mejores y más dulces frutas de estación", precio: 600, talle: "-", img: 'https://i.ibb.co/yYpW8zM/IMG-20210818-183758.jpg', cantidad:1},
+    
+]
+
